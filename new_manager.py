e52c3475893474dcc136d5aedfb5950ce73fe74a
effi-cacious <effi-cacious@mailbox.org>
2023-09-20 09:48:22 -0500

    added table definitions

--- new_manager.py
+++ new_manager.py
@@ -27,22 +27,41 @@
 
 ### create tables in file DB
 
-### table checksum
+### table checksums
 # |id|checksum|date added|
 # checksum: string sha256
 # date added: date time
+#
+# CREATE TABLE "checksums" (
+# 	"id"	INTEGER UNIQUE,
+# 	"checksum"	TEXT NOT NULL,
+# 	"date_added"	INTEGER DEFAULT 0,
+# 	PRIMARY KEY("id" AUTOINCREMENT)
+# );
 
 ### table file info
 # |id|checksum_id|file name|file path|mime type id|file size|duplicate|duplicate id|target device id|session id|
-# checksum id: foreign key
 # file name: string with extension
 # file path: string, relative path
-# mime-type id: foreign key
 # file size: int in Byte
 # duplicate: int 0=False, 1=True
-# duplicate id: foreign key
-# target device id: foreign key
+# checksum id: foreign key
+# mime-type id: foreign key
 # session id: foreign key
+# target device id: foreign key
+#
+# CREATE TABLE "file_info" (
+# 	"id"	INTEGER UNIQUE,
+# 	"file_name"	TEXT NOT NULL,
+# 	"file_path"	TEXT NOT NULL,
+# 	"file_size"	INTEGER DEFAULT 0,
+# 	"duplicate"	INTEGER DEFAULT 0,
+# 	"fk_checksums"	INTEGER,
+# 	"fk_devices"	INTEGER,
+# 	"fk_mime-types"	INTEGER,
+# 	"fk_sessions"	INTEGER,
+# 	PRIMARY KEY("id" AUTOINCREMENT)
+# );
 
 ### table device
 # |id|device serial|space used|size|space free|
@@ -51,10 +70,26 @@
 # size: int in MByte
 # space free: int in MByte
 
+# CREATE TABLE "devices" (
+# 	"id"	INTEGER UNIQUE,
+# 	"dev_serial"	TEXT NOT NULL,
+# 	"size"	INTEGER,
+# 	"space_used"	INTEGER,
+# 	"space_free"	INTEGER,
+# 	PRIMARY KEY("id" AUTOINCREMENT)
+# );
+
 ### table MIME type
 # |id|type name|type category|
 # type name: string
 # type category: string
+
+# CREATE TABLE "mime_types" (
+# 	"id"	INTEGER UNIQUE,
+# 	"name"	TEXT UNIQUE,
+# 	"category"	TEXT,
+# 	PRIMARY KEY("id" AUTOINCREMENT)
+# );
 
 ### table sessions
 # |id|start|ended|source device id|target device id|size|size copied|size freeed|
@@ -67,9 +102,29 @@
 # length: int in seconds
 # user: foreign key
 
+# CREATE TABLE "sessions" (
+# 	"id"	INTEGER UNIQUE,
+# 	"starting_time"	INTEGER,
+# 	"ending_time"	INTEGER,
+# 	"size_copied"	INTEGER,
+# 	"size_freed"	INTEGER,
+# 	"length"	INTEGER,
+# 	"fk_users"	INTEGER,
+# 	"fk_devices"	INTEGER,
+# 	PRIMARY KEY("id" AUTOINCREMENT)
+# );
+
 ### table user
 # |id|name|
 # name: string
+
+# CREATE TABLE "users" (
+# 	"id"	INTEGER UNIQUE,
+# 	"username"	TEXT UNIQUE,
+# 	"email"	TEXT,
+# 	"password"	TEXT,
+# 	PRIMARY KEY("id" AUTOINCREMENT)
+# );
 
 ### test file DB for write and read operations
 
@@ -81,15 +136,33 @@
 
 #---------#
 
-# get current user by whoamior other means
+# get current user by whoami or other means
 
 # calculate session size via du -h of source path
 
-# estimate session length and display to user
+# estimate session length and display to user - that can be used to display a percentage as well
 
 # if more than one target is specified get all free space and compare session size to that
 
 # advice user that not all files can be copied if the session size is larger than the target free space
+
+# add session info to DB
+
+## table device (if it is not in DB)
+## serial no
+## size in Mbyte
+## space used in MByte
+## space free in MByte
+
+## table session
+## - start time
+## - end time
+## - source device id
+## - size = 0
+## - size copied = 0
+## - size freeed = 0
+## - length = 0
+## - user id
 
 #---------#
 
@@ -114,9 +187,44 @@
 ### - checksum
 ### - datetime
 
+### table file info
+### - file name
+### - file path
+### - file size in Byte
+### - checksum id
+### - device id
+### - duplicate = 0
+### - mime type id
+### - session id of current session
+### - target device id
+
+### table device id (if doesn't exist):
+### - device serial
+
 ### table mime type (if mime type new:
 ### - name
 ### - category
+
+### table session
+### - increment size
+### - increment size copied/freeed
+
+###--------###
+
+### YES: do not copy to target
+
+### TRUE: add info DB
+
+### table file info
+### - file name
+### - file path
+### - file size in Byte
+### - duplicate = 1
+### - checksum id
+### - device id
+### - mime type id
+### - session id of current session
+### - target device id
 
 ### table device id (if doesn't exist):
 ### - device serial
@@ -125,20 +233,13 @@
 ### - increment size
 ### - increment size copied/freeed
 
-### table file info
-### - file name
-### - file path
-### - device id
-### - duplicate = FALSE
-### - mime type
-### - file size
-### - target device id
-### -
-
 ###--------###
+##--------##
+#--------#
 
-### YES: do not copy to target
 
-### YES: check if file name already exists
+# close session by updating table session
+## - ended
+## - length
 
-#### TRUE: add info DB - file path path, device id, mark as duplicate, duplicate id from already existing file
+# print summary
